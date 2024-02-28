de48cd55e30d70158a783fa2386eef07739b72cd
Anushka <134141655+anushkaaa1@users.noreply.github.com>
2024-02-28 20:55:43 +0600

    Add files via upload

---
+++ doublylist.java
@@ -0,0 +1,47 @@
+public class doublylist {
+    Node head,tail;
+
+    public doublylist(){
+        head = tail = null;
+    }
+    public class Node {
+        int  data;
+        Node next;
+        Node prev;
+        
+        Node(int data){
+            this.data= data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public void create(int value){
+        Node newnode = new Node(value);
+        if(head==null){
+            head = tail = newnode;
+        }
+        else{
+            newnode.prev = tail;
+            tail.next = newnode;
+            tail = newnode;
+        }
+    }
+    public void display(Node head){
+        if(head==null){
+            System.out.println("list is empty");
+        }
+        else{
+            while (head!=null) {
+                System.out.println(head.data);
+                head = head.next;
+            }
+        }
+    }
+    public static void main(String[] args){
+        doublylist list = new doublylist();
+        list.create(2);
+        list.create(4);
+        list.create(6);
+        list.display(list.head);
+    }
+}
